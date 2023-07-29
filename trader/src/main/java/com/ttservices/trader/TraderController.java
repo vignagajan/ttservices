e067467c72186113df65503566cd5fbba3cdf5a4
vignagajan <my.soclntwrks@gmail.com>
2023-07-29 13:47:36 +0000

    feat(trader): add trader controller

---
+++ trader/src/main/java/com/ttservices/trader/TraderController.java
@@ -0,0 +1,18 @@
+package com.ttservices.trader;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("api/v1/traders")
+public record TraderController {
+
+    @PostMapping
+    public void registerTrader(@RequestBody TraderRequest traderRequest){
+        log.info("New trader registration: {}",traderRequest);
+    }
+}
