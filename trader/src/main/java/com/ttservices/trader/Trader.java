7b269a0d253db532ecd844690fa1f7c560a2f777
vignagajan <my.soclntwrks@gmail.com>
2023-08-02 10:14:18 +0000

    feat(trader): integerate model with JPA

--- trader/src/main/java/com/ttservices/trader/Trader.java
+++ trader/src/main/java/com/ttservices/trader/Trader.java
@@ -1,12 +1,31 @@
 package com.ttservices.trader;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.SequenceGenerator;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Trader {
 
+    @Id
+    @SequenceGenerator(
+            name="trader_id_sequence",
+            sequenceName = "trader_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "trader_id_sequence",
+    )
     private String Id;
     private String firstname;
     private String lastname;
