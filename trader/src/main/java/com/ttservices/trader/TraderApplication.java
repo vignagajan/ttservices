39dc05612bf64048f007419aa7ab618a276d6fa2
vignagajan <my.soclntwrks@gmail.com>
2023-08-27 15:33:19 +0000

    chore(trader): connect application with eureka

--- trader/src/main/java/com/ttservices/trader/TraderApplication.java
+++ trader/src/main/java/com/ttservices/trader/TraderApplication.java
@@ -2,8 +2,10 @@ package com.ttservices.trader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TraderApplication {
     public static void main(String[] args) {
         SpringApplication.run(TraderApplication.class,args);
